1f79195473fc270989f8e6c913011c860c1d091f
sunjinwei <sunjinwei5828@163.com>
2019-07-22 11:03:51 +0800

    sun-1.0

--- itheima-cas-security/src/main/java/cn/itcast/demo/service/UserDetailServiceImpl.java
+++ itheima-cas-security/src/main/java/cn/itcast/demo/service/UserDetailServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public class UserDetailServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println(111);
         System.out.println("----------------------------username");
         //方法 之前我们是用来 从数据库查询用户的信息进行认证权限的控制（1.权限 2.认证）
         //现在和CAS集成，该方法就只做一件事：就是授权。认证交给CAS服务端
