83c466d8727e86d488c30381c7d8ed0bab10d04c
leilvlong <1455013965@qq.com>
2019-07-25 10:15:47 +0800

    excel上传分类数据

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/ItemCatService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/ItemCatService.java
@@ -34,4 +34,7 @@ public interface ItemCatService extends CoreService<TbItemCat> {
     void updateStatus(Long[] ids, String status);
 
     void updateStatus(Long[] ids);
+
+    void insertAll(List<TbItemCat> itemCats);
+
 }
